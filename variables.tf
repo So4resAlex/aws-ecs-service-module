67556cde382ca3cc11298e3fdb4d9e5e5df11283
Alex_Soares <alealvessoares28@gmail.com>
2024-09-14 23:45:36 -0300

    feat: add some resources

---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "region" {} #TO-DO 
+
+variable "service_name" {}
+
+variable "cluster_name" {}
+
+variable "vpc_id" {}
+
+variable "private_subnets" {}
+
+variable "service_port" {}
+
+variable "service_cpu" {}
+
+variable "service_memory" {}
+
+variable "service_listener" {}
+
+variable "service_task_execution_role" {}
+
+variable "service_launch_type" {}
+
+variable "service_task_count" {}
+
+variable "service_hosts" {}
+
+variable "service_healthcheck" {
+  type = map(any)
+
+}
+
+variable "environment_variables" { #TO-DO 
+  type = list(any)
+}
+
+variable "capabilities" { #TO-DO 
+  type = list(any)
+}
