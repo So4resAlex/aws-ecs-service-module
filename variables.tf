6c98ee0561af3d4157689cac581da784dbf18f79
Alex_Soares <alealvessoares28@gmail.com>
2024-09-17 07:41:31 -0300

    feat: add autoscaling vars

--- variables.tf
+++ variables.tf
@@ -36,3 +36,25 @@ variable "environment_variables" { #TO-DO
 variable "capabilities" { #TO-DO 
   type = list(any)
 }
+
+variable "scale_type" {}
+
+variable "task_minimum" {}
+
+variable "task_maximum" {}
+
+#### Auto Scaling de cpu
+
+variable "scale_out_cpu_threshold" {}
+
+variable "scale_out_adjustment" {}
+
+variable "scale_out_comparison_operator" {}
+
+variable "scale_out_statistic" {}
+
+variable "scale_out_period" {}
+
+variable "scale_out_evaluation_periods" {}
+
+variable "scale_out_cooldown" {}
