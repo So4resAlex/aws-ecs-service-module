f5e4e38f8267d7ccb1a3da983d95b34d7f5956a9
Alex_Soares <alealvessoares28@gmail.com>
2024-10-09 19:21:53 -0300

    fix: typo

--- task_definitions.tf
+++ task_definitions.tf
@@ -16,7 +16,7 @@ resource "aws_ecs_task_definition" "main" {
 
       essential = true
 
-      portMapping = [{
+      portMappings = [{
         containerPort = var.service_port
         hostPort      = var.service_port
         protocol      = "tcp"
